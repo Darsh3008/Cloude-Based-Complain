88d6efacd0ad956713e520317751840987d25976
Darsh3008 <darshkanani326@gmail.com>
2026-07-06 12:26:16 +0530

    fix: update baseURL in axios instance to point to the correct deployment URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cloude-based-complain.onrender.com/api',
+  baseURL: 'https://cloude-based-complain-git-main-darshkanani326-5720s-projects.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
